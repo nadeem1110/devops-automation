f7798af828ca474208ac9140bc1844cd478f9529
nadeem1110 <103575023+nadeem1110@users.noreply.github.com>
2022-07-22 17:18:50 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Hello from Nadeem ";
+		return "Hello from Nadeem Patel ";
 	}
 
 	public static void main(String[] args) {
