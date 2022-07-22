f733360e98580cc693b780df985d058abda4344d
nadeem1110 <103575023+nadeem1110@users.noreply.github.com>
2022-07-22 15:23:40 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -1,5 +1,3 @@
-package com.javatechie;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
